e030c12a253af49aac0eeab29c7d9bef1927375d
EstebanAG1005 <60299338+EstebanAG1005@users.noreply.github.com>
2022-11-28 18:40:21 -0600

    Update

--- Materiales/ray.py
+++ Materiales/ray.py
@@ -86,48 +86,90 @@ class Raytracer(object):
 
 rubber = Material(diffuse=color(80, 0, 0), albedo=(0.6, 0.3), spec=50)
 ivory = Material(diffuse=color(100, 100, 80), albedo=(0.9, 0.1), spec=10)
-coffee = Material(diffuse=color(170, 80, 40), albedo=(0.9, 0.3), spec=7)
-softcoffee = Material(diffuse=color(230, 170, 135), albedo=(0.9, 0.9), spec=35)
-dark = Material(diffuse=color(0, 0, 0), albedo=(0.3, 0.3), spec=3)
+cafe = Material(diffuse=color(170, 80, 40), albedo=(0.9, 0.3), spec=7)
+cafe_claro = Material(diffuse=color(230, 170, 135), albedo=(0.9, 0.9), spec=35)
+negro = Material(diffuse=color(0, 0, 0), albedo=(0.3, 0.3), spec=3)
 lightGreen = Material(diffuse=color(130, 223, 36), albedo=(0.9, 0.9), spec=10)
 iron = Material(diffuse=color(200, 200, 200), albedo=(1, 1), spec=20)
 snow = Material(diffuse=color(250, 250, 250), albedo=(0.9, 0.9), spec=35)
 gold = Material(diffuse=color(255, 215, 0), albedo=(0.9, 0.9), spec=35)
+red = Material(diffuse=color(255, 0, 0), albedo=(0.9, 0.9), spec=35)
+plata = Material(diffuse=color(192, 192, 192), albedo=(0.9, 0.9), spec=35)
 
 r = Raytracer(800, 600)
 r.light = Light(V3(-20, 0, 20), 1)
 r.scene = [
     # OSO CAFE (derecha)
     # cuerpo y adorno
-    Sphere(V3(2.5, -1, -10), 1.5, rubber),
+    Sphere(V3(2.5, -1, -10), 1.5, red),
     Sphere(V3(2.2, 0.2, -8.6), 0.2, lightGreen),
     Sphere(V3(1.8, 0.2, -8.6), 0.17, lightGreen),
     Sphere(V3(2.5, 0.2, -8.6), 0.17, lightGreen),
-    Sphere(V3(2.5, -0.2, -8.6), 0.17, gold),
-    Sphere(V3(2.0, -0.2, -8.6), 0.17, gold),
-    Sphere(V3(1.8, -0.2, -8.6), 0.17, gold),
+    Sphere(V3(2.8, 0.2, -8.6), 0.17, lightGreen),
+    Sphere(V3(1.5, 0.2, -8.6), 0.17, lightGreen),
+    # Listones dorador
     Sphere(V3(1.6, -0.2, -8.6), 0.17, gold),
+    Sphere(V3(1.3, -0.2, -8.6), 0.17, gold),
+    # Listones dorador
+    Sphere(V3(1.9, -0.4, -8.6), 0.17, gold),
+    Sphere(V3(1.6, -0.4, -8.6), 0.17, gold),
+    # Listones dorador
+    Sphere(V3(2.2, -0.6, -8.6), 0.17, gold),
+    Sphere(V3(1.9, -0.6, -8.6), 0.17, gold),
+    # Cuarta parte
+    Sphere(V3(2.5, -0.8, -8.6), 0.17, gold),
+    Sphere(V3(2.2, -0.8, -8.6), 0.17, gold),
+    # Quinta parte
+    Sphere(V3(2.8, -1, -8.6), 0.17, gold),
+    Sphere(V3(2.5, -1, -8.6), 0.17, gold),
+    # Sexta parte
+    Sphere(V3(3.1, -1.2, -8.6), 0.17, gold),
+    Sphere(V3(2.8, -1.2, -8.6), 0.17, gold),
     # cabeza
-    Sphere(V3(2.5, 1.5, -10), 1.25, softcoffee),
+    Sphere(V3(2.5, 1.5, -10), 1.25, cafe_claro),
     # osico y orejas
-    Sphere(V3(2.3, 1.1, -9), 0.4, coffee),
-    Sphere(V3(3.4, 2.3, -9), 0.35, coffee),
-    Sphere(V3(1.4, 2.3, -9), 0.35, coffee),
+    Sphere(V3(2.3, 1.1, -9), 0.4, cafe),
+    Sphere(V3(3.4, 2.3, -9), 0.35, cafe),
+    Sphere(V3(1.4, 2.3, -9), 0.35, cafe),
     # extremidades
-    Sphere(V3(4, 0, -10), 0.45, softcoffee),
-    Sphere(V3(1, 0, -10), 0.45, softcoffee),
-    Sphere(V3(4, -2.2, -10), 0.5, softcoffee),
-    Sphere(V3(1, -2.2, -10), 0.5, softcoffee),
+    Sphere(V3(4, 0, -10), 0.45, cafe_claro),
+    Sphere(V3(1, 0, -10), 0.45, cafe_claro),
+    Sphere(V3(4, -2.2, -10), 0.5, cafe_claro),
+    Sphere(V3(1, -2.2, -10), 0.5, cafe_claro),
     # nariz y ojos
-    Sphere(V3(2, 1, -8), 0.1, dark),
-    Sphere(V3(2.3, 1.5, -8), 0.1, dark),
-    Sphere(V3(1.7, 1.5, -8), 0.1, dark),
+    Sphere(V3(2, 1, -8), 0.1, negro),
+    Sphere(V3(2.3, 1.5, -8), 0.1, negro),
+    Sphere(V3(1.7, 1.5, -8), 0.1, negro),
     # OSO BLANCO (izquierda)
     # cuerpo y adorno
     Sphere(V3(-2.5, -1, -10), 1.5, snow),
-    Sphere(V3(-2.2, 0.2, -8.6), 0.2, rubber),
-    Sphere(V3(-1.9, 0.2, -8.6), 0.17, rubber),
-    Sphere(V3(-2.5, 0.2, -8.6), 0.17, rubber),
+    Sphere(V3(-2.2, 0.2, -8.6), 0.2, red),
+    Sphere(V3(-1.9, 0.2, -8.6), 0.17, red),
+    Sphere(V3(-2.5, 0.2, -8.6), 0.17, red),
+    Sphere(V3(-2.8, 0.2, -8.6), 0.17, red),
+    # Cintas Plateadas
+    Sphere(V3(-2.2, -0.2, -8.6), 0.2, plata),
+    Sphere(V3(-1.9, -0.2, -8.6), 0.17, plata),
+    Sphere(V3(-2.5, -0.2, -8.6), 0.17, plata),
+    Sphere(V3(-2.8, -0.2, -8.6), 0.17, plata),
+    Sphere(V3(-3.1, -0.2, -8.6), 0.17, plata),
+    Sphere(V3(-1.6, -0.2, -8.6), 0.17, plata),
+    # Cintas Plateadas
+    Sphere(V3(-2.2, -0.8, -8.6), 0.2, plata),
+    Sphere(V3(-1.9, -0.8, -8.6), 0.17, plata),
+    Sphere(V3(-2.5, -0.8, -8.6), 0.17, plata),
+    Sphere(V3(-2.8, -0.8, -8.6), 0.17, plata),
+    Sphere(V3(-3.1, -0.8, -8.6), 0.17, plata),
+    Sphere(V3(-1.6, -0.8, -8.6), 0.17, plata),
+    Sphere(V3(-3.4, -0.8, -8.6), 0.17, plata),
+    Sphere(V3(-1.3, -0.8, -8.6), 0.17, plata),
+    # Cintas Plateadas
+    Sphere(V3(-2.2, -1.6, -8.6), 0.2, plata),
+    Sphere(V3(-1.9, -1.6, -8.6), 0.17, plata),
+    Sphere(V3(-2.5, -1.6, -8.6), 0.17, plata),
+    Sphere(V3(-2.8, -1.6, -8.6), 0.17, plata),
+    Sphere(V3(-3.1, -1.6, -8.6), 0.17, plata),
+    Sphere(V3(-1.6, -1.6, -8.6), 0.17, plata),
     # cabeza
     Sphere(V3(-2.5, 1.5, -10), 1.25, snow),
     # osico y orejas
@@ -140,9 +182,9 @@ r.scene = [
     Sphere(V3(-4, -2.2, -10), 0.5, snow),
     Sphere(V3(-1, -2.2, -10), 0.5, snow),
     # nariz y ojos
-    Sphere(V3(-2.1, 1, -8), 0.1, dark),
-    Sphere(V3(-2.4, 1.5, -8), 0.1, dark),
-    Sphere(V3(-1.8, 1.5, -8), 0.1, dark),
+    Sphere(V3(-2.1, 1, -8), 0.1, negro),
+    Sphere(V3(-2.4, 1.5, -8), 0.1, negro),
+    Sphere(V3(-1.8, 1.5, -8), 0.1, negro),
 ]
 
 r.render()
